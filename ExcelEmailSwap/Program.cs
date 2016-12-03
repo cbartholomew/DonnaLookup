8ffb00dda02098aaf3e552723be2f66b8f119984
Christopher Bartholomew <cbartholomew@gmail.com>
2016-12-03 10:08:45 -0800

    updates

---
+++ ExcelEmailSwap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelEmailSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceDocument = "C:\\Users\\Christopher\\Documents\\Temp\\ACTIVE EMAIL LIST.xlsx";
+            string sourceReplacement = "C:\\Users\\Christopher\\Documents\\Temp\\OutputFINAL.xlsx";
+            List<string[]> sourceReplacementColumns
+            = uwExcel.createFromExcel(sourceReplacement, "A", "D", "Sheet1");
+
+            // 37 - 44
+            foreach (string[] sourceReplacementRow in sourceReplacementColumns)
+            {
+                var sourceReplacementEmail = sourceReplacementRow[1];
+
+                uwExcel.replaceFromExcel(sourceDocument, "A", "CC", sourceReplacementEmail,"","Sheet1");                
+            }
+        }
+    }
+}
